05eb56b64aab8f5f396537d2a5eee47dde781018
samade747 <samad.e747@gmail.com>
2023-08-27 17:31:25 +0500

    stopswitch made

---
+++ app.js
@@ -0,0 +1,45 @@
+// creating stopSwitch
+
+var minutes = 0 
+var seconds = 0
+var miliseconds = 0
+var getmin = document.getElementById('min')
+var getsec = document.getElementById('sec')
+var getmili = document.getElementById('msec')
+var interval;
+
+
+function start(){
+    interval = setInterval(function(){
+        miliseconds++
+        getmili.innerHTML = miliseconds
+        if(miliseconds >= 100){
+            seconds++
+            getsec.innerHTML = seconds
+            miliseconds = 0
+        }
+        else if(seconds >= 60){
+            minutes++
+            getmin.innerHTML = minutes
+            seconds = 0
+        }
+    
+    },100)
+    document.getElementById('sss').disabled = true
+    
+}
+
+function stop(){
+    clearInterval(interval)
+    document.getElementById('sss').disabled = false
+
+}
+
+function reset(){
+    minutes = 0
+    miliseconds = 0
+    seconds = 0
+    getmin.innerHTML = minutes
+    getsec.innerHTML = seconds
+    getmili.innerHTML = miliseconds
+}
